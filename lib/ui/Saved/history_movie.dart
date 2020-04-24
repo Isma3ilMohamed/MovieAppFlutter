90427c709c730472a26d7d1448fa6c73e63e2cc7
Ismail Mohamed <thedevwolf@gmail.com>
2020-04-24 16:27:01 +0300

    add delete slidable

--- lib/ui/Saved/history_movie.dart
+++ lib/ui/Saved/history_movie.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:movieapp/data/database/MovieModel.dart';
 import 'package:movieapp/ui/items/fav_item.dart';
 import 'package:movieapp/util/DbHelper.dart';
@@ -14,7 +16,6 @@ class HistoryMovies extends StatefulWidget {
 class HistoryState extends State<HistoryMovies> {
   List<MovieDb> movies;
 
-
   @override
   void initState() {
     if (movies == null) {
@@ -26,8 +27,6 @@ class HistoryState extends State<HistoryMovies> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       body: Center(
         child: _isAnyItems()
@@ -46,8 +45,37 @@ class HistoryState extends State<HistoryMovies> {
   Widget _buildListView() {
     return ListView.builder(
       itemBuilder: (context, position) {
-        return FavItem(
-          movie: this.movies[position],
+        /* return Dismissible(
+          key: Key(movies[position].id.toString()),
+          child: FavItem(
+            movie: this.movies[position],
+          ),
+          onDismissed: (direction){
+
+          },
+          background: Container(color: Colors.red,),
+          secondaryBackground: Container(color: Colors.green,),
+          direction: DismissDirection.endToStart,
+
+        );*/
+        return Slidable(
+          direction: Axis.horizontal,
+          actionPane: SlidableDrawerActionPane(),
+          actionExtentRatio: 0.25,
+          child: FavItem(
+            movie: this.movies[position],
+          ),
+          actions: <Widget>[
+            IconSlideAction(
+              caption: 'Delete',
+              color: Colors.red,
+              icon: Icons.delete,
+              onTap: () {
+                removeMovieFromFavorite(position);
+
+              },
+            )
+          ],
         );
       },
       itemCount: movies.length,
@@ -57,8 +85,16 @@ class HistoryState extends State<HistoryMovies> {
 
   void _getData() async {
     var result = await helper.getMovies();
-    setState(()  {
+    setState(() {
       movies = result;
+    });
+  }
+
+  void removeMovieFromFavorite(int position) {
+    helper.deleteMovie(movies[position].id).then((result) {
+      setState(() {
+        movies.removeAt(position);
+      });
     });
   }
 }
